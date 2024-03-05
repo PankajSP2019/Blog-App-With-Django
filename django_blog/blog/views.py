b7b7e17c797d5584b7efc7463af3e0e9380e382c
Pankaj <pankajds12345@gmail.com>
2024-03-05 22:40:53 +0600

    first commit

---
+++ django_blog/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def blogHome(request):
+    return render(request, "blog/bloghome.html")
+
+
+def blogPost(request, slug):
+    return render(request, "blog/blogpost.html", {'text': slug})
