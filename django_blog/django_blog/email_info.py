118a125a0eeac36f828488c6f3a64482824e6dfd
Pankaj <pankajds12345@gmail.com>
2024-04-10 20:42:02 +0600

    first commit

--- django_blog/django_blog/email_info.py
+++ django_blog/django_blog/email_info.py
@@ -3,5 +3,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'todoapp611@gmail.com'  # Your Host Mail
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'ipiowigaabrjlcoo'
 EMAIL_PORT = 587
--- django_blog/static/js/tinyInject.js
+++ django_blog/static/js/tinyInject.js
@@ -29,7 +29,7 @@
 
 var script= document.createElement('script');
 script.type='text/javascript';
-script.src="https://cdn.tiny.cloud/1//tinymce/5/tinymce.min.js";
+script.src="https://cdn.tiny.cloud/1/nlqp8tthxp2bcdc48fll72m1cjiliwnttuy4ae0g5liarklk/tinymce/5/tinymce.min.js";
 script.referrerpolicy="origin"
 
 document.head.appendChild(script);
@@ -39,7 +39,7 @@ tinymce.init({
     selector: "#id_content",
     height:656,
     plugins: [
-        'advlist autolink link image lists charmap print hr anchor pagebreak',
+        'advlist autolink preview link image lists charmap print hr anchor pagebreak',
         'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
         'table emoticons template paste help'
       ],
